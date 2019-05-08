e535f7aa76726dff8b5fea75c009bf9c9fe316fa
dy86 <devyang@qq.com>
2019-05-08 15:02:16 +0800

    init

---
+++ src/components/UserPhoto.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="photo">头像</div>
+    <div class="name">姓名：{{name}}</div>
+    <div class="no">工号：{{no}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "UserPhoto",
+    props : {
+      name: String,
+      no: String
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
